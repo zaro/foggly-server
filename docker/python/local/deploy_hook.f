5ae05f16524999b33abf82e0971b7e198ca3f754
Svetlozar Argirov <zarrro@gmail.com>
2016-02-14 10:01:56 +0200

    Fix some problems with deployment on startup in python and nodejs containers

--- docker/python/local/deploy_hook.f
+++ docker/python/local/deploy_hook.f
@@ -3,11 +3,17 @@
 function activate_pyvenv {
 
   if [ ! -d /srv/home/pyvenv ]; then
-    pyvenv /srv/home/pyvenv
+    pyvenv-3.5 /srv/home/pyvenv
   fi
 
   . /srv/home/pyvenv/bin/activate
 
+}
+
+function procfile_entry {
+  if [[ "$1" != "web" ]]; then
+    procfile_entry_to_supervisor "$@"
+  fi
 }
 
 function deploy_hook_init {
@@ -23,5 +29,10 @@ function deploy_hook_install {
 }
 
 function deploy_hook_reload {
+  . /usr/local/procfile_to_supervisor
+
+  /usr/bin/supervisorctl reread
+  /usr/bin/supervisorctl update
+
   /usr/bin/supervisorctl restart web
 }
