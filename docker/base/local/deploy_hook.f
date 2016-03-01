cc19544127bfe48cc1ae6bd1abed3496cee86cdf
Svetlozar Argirov <zarrro@gmail.com>
2016-03-01 10:50:12 +0200

    deploy hooks fixes

--- docker/base/local/deploy_hook.f
+++ docker/base/local/deploy_hook.f
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function deploy_hook_init {
-
+  return
 }
 
 function deploy_hook_install {
-
+  return
 }
 
 function deploy_hook_reload {
-
+  return
 }
--- docker/php7/local/deploy_hook.f
+++ docker/php7/local/deploy_hook.f
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 function deploy_hook_init {
+  return
 }
 
 # More info on how composer install vs update work:
 #    https://adamcod.es/2013/03/07/composer-install-vs-composer-update.html#fn2
 function deploy_hook_install {
-  echo '*** BEGIN composer install in '`pwd`
-  /usr/local/bin/composer install
-  echo '*** END composer install'
+  if [[ -f  composer.json ]]; then
+    echo '*** BEGIN composer install in '`pwd`
+    /usr/local/bin/composer install
+    echo '*** END composer install'
+  fi
 }
 
 function deploy_hook_reload {
+  return
 }
