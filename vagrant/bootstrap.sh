12bf1a7224368e0a551598bf8c16b8fc52ee0ad9
Svetlozar Argirov <zarrro@gmail.com>
2017-03-11 11:53:43 +0200

    Vagrant bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
 apt-get update -y
-apt-get install -y --force-yes curl nodejs mariadb-server python3-pip python3-docker python3-jinja2 python3-mysql.connector nginx firewalld
-
-pip3 install celery
-pip3 install redis
-pip3 install hiredis
+apt-get install -y --force-yes curl mariadb-server  nginx firewalld postgresql libpq-dev
 
 systemctl enable mariadb
 systemctl start mariadb
 
-ln -sf /usr/bin/python3.5 /usr/bin/python3
-ln -s /usr/bin/nodejs /usr/bin/node
+systemctl enable postgresql
+systemctl start postgresql
+
 
 curl -fsSL https://get.docker.com/ | sh
 
 sudo usermod -aG docker vagrant
-
-mkdir -p /etc/systemd/system/docker.service.d/
-cat <<EOS >/etc/systemd/system/docker.service.d/override.conf
-[Service]
-ExecStart=
-ExecStart=/usr/bin/docker daemon -H fd:// -H tcp://0.0.0.0:2375
-EOS
 
 systemctl enable docker
 systemctl start docker
